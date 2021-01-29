debf8080db7fb066a557df3bea85819b09c1ad7a
ivan123 <70339505+ivan123gm@users.noreply.github.com>
2021-01-30 01:07:19 +0300

    SomeRandomRobot 1.0.1

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Discord bot, which i created for learning JavaScript.
 2. Clone this repo
 3. In folder "some-random-bot" open "bot-src" folder.
 4. Execute command "npm install discord.js"
-5. Replace "insert-your-token-here" with your Discord bot token
+5. Replace "insert-your-token-here" with your Discord bot token in config.json
 6. Execute command "node ."
 
 # If you wanna redistribute this bot, then upload your own version of my bot on Github and replace these lines in package.json file, please.
+Note: while uploading your own version of this bot, do not upload node_modules folder.
 ![replace lines, which i highlighted in red.](/images/img1.png)
--- bot-src/index.js
+++ bot-src/index.js
@@ -1,26 +1,31 @@
-
+//storing all consts
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
+const {prefix, token } = require('./config.json');
+//login with token in config.json
+client.login(token);
 
-const prefix = '.';
-
-client.login('insert-your-token-here');
-
+//logging that bot has been launched in console
 client.once('ready', () => {
  console.log('Hey, this bot is online now!');
   });
 
+//splitting command and arguments (just in case if i gonna add bot commands arguments.)
   client.on('message', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
-
+    
     if (command === 'ping')
       {message.reply('pong!')}
-
+    
       else if (command === 'hello')
-        {message.reply('hello!')};
+        {message.reply('hello!')}
 
+      else if (command === 'bruh')
+        {message.reply('bruh')}
+        
+      else if (command === 'help')
+      {message.channel.send (`Prefix: "." \nCommands: ping, hello, bruh, help`)}
 });
