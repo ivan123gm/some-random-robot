f37f13ec7c2fb773cee98be08cea658417a64618
ivan123 <70339505+ivan123gm@users.noreply.github.com>
2021-01-30 01:43:36 +0300

    SomeRandomRobot 1.0.2
    Change log:
    Added "help" command
    Added "serverinfo" command
    Small fixes of code

--- bot-src/index.js
+++ bot-src/index.js
@@ -1,21 +1,21 @@
 //storing all consts
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const {prefix, token } = require('./config.json');
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const {prefix, token } = require('./config.json')
 //login with token in config.json
-client.login(token);
+client.login(token)
 
 //logging that bot has been launched in console
 client.once('ready', () => {
- console.log('Hey, this bot is online now!');
+ console.log('Hey, this bot is online now!')
   });
 
 //splitting command and arguments (just in case if i gonna add bot commands arguments.)
   client.on('message', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase()
     
     if (command === 'ping')
       {message.reply('pong!')}
@@ -27,5 +27,9 @@ client.once('ready', () => {
         {message.reply('bruh')}
         
       else if (command === 'help')
-      {message.channel.send (`Prefix: "." \nCommands: ping, hello, bruh, help`)}
+      {message.channel.send (`Prefix: "." \nCommands: ping, hello, bruh, help, serverinfo`)}
+
+      else if (command === 'serverinfo')
+      {message.channel.send (`Server name: ${message.guild.name}\nAmount of members: ${message.guild.memberCount}\nServer creation info: ${message.guild.createdAt}\nServer region: ${message.guild.region}`)
+    }
 });
